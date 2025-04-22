a30466663af2755e293a08460344c4669c2d8278
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-04-22 02:14:19 -0300

    mudar url da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080' // Porta do backend local
-  });
+  baseURL: 'https://startupback.onrender.com' // nova URL do backend
+})
 
-export default api;
+export default api
