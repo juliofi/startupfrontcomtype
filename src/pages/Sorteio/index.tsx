fd5a25b64c70f9df657d82e1df190b6ba490e147
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-04-21 22:14:20 -0300

    fix pro deploy

--- src/pages/Sorteio/index.tsx
+++ src/pages/Sorteio/index.tsx
@@ -102,9 +102,6 @@ export default function Sorteio() {
   }, [batalhas])
 
 
-
-  const algumaFinalizada = batalhas.some(b => b.finalizada)
-
   return (
     <div className={styles.container}>
       <h1 className={styles.titulo}>Batalhas</h1>
